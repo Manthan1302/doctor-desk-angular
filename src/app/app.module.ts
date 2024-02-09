d3055a0bf792658e416d5d674a8c928eb5772e72
Manthan Thakkar <Manthant@btl.com>
2024-02-09 15:29:59 +0530

    start new project Doctor-Desk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { PatientComponent } from './patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
