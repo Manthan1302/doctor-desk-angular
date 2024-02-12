c8c858825b042be4fd2ca381354f086fcaf6f20f
Manthan Thakkar <Manthant@btl.com>
2024-02-12 17:55:13 +0530

    add edit profile function

--- src/app/patient/PatientService/common-service.service.ts
+++ src/app/patient/PatientService/common-service.service.ts
@@ -3,17 +3,33 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { DoctorRegisterService } from 'src/app/doctor/DoctorServices/DoctorRegister.service';
 import { DoctorRegistration } from 'src/app/doctor/doctorModel/DoctorRegistrationModel';
+import { PatientRegistration } from '../PatientModel/PatientRegistartionModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonServiceService {
 
-
+  loggedPatient!: PatientRegistration;
+  id: number | null = null;
   constructor(private httpClient: HttpClient, private router: Router) { }
-  getPatient: string = "http://localhost:3000/allDoctors"
-  
-  getAllDoctors(){
-    return this.httpClient.get<DoctorRegistration[]>(this.getPatient)
+  url: string = "http://localhost:3000"
+
+  getAllDoctors() {
+    const path = this.url + '/allDoctors';
+    return this.httpClient.get<DoctorRegistration[]>(path)
+  }
+
+  updatePatientProfile(data: any) {
+    const patient = sessionStorage.getItem('loggedPatient')
+    if (patient) {
+      let patients = JSON.parse(patient)
+      this.loggedPatient = data
+      this.loggedPatient.PatientGender = patients.PatientGender;
+      this.id = patients.id
+    }
+    const path = `${this.url}/allPatient/${this.id}`;
+    return this.httpClient.put(path, this.loggedPatient)
+
   }
 }
